4d6330d244559d19019ebc5447d75d974a3702d3
lpp12138 <lpp12138@outlook.com>
2021-09-07 19:08:13 +0800

    初次上传,实现关键词检测和回复,添加指令的功能(

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using jsonDataStructs;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+
+namespace QBot
+{
+    class Program
+    {
+        static IEnumerable<string> files;
+        static List<string> keyWord = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to Qbot");
+            bot();
+            while (true) ;
+        }
+        async static public void bot()
+        {
+            ColorWriteLine($"CurrentDir:{Directory.GetCurrentDirectory()}",ConsoleColor.Yellow);
+            refreshFiles();
+            String cqUri = "ws://127.0.0.1:6700/";
+            ClientWebSocket cWS = new ClientWebSocket();
+            await cWS.ConnectAsync(new Uri(cqUri), CancellationToken.None);
+            ColorWriteLine("websocket connected", ConsoleColor.Green);
+            while (true)
+            {
+                Thread.Sleep(10);
+                try
+                {
+                    byte[] receiveBuffer = new byte[4096];
+                    ArraySegment<byte> cWsReceiveBuffer = new ArraySegment<byte>(receiveBuffer);
+                    CancellationToken cts = new CancellationToken();
+                    WebSocketReceiveResult webSocketReceiveResult = await cWS.ReceiveAsync(cWsReceiveBuffer, cts);
+                    string cWsReceivedData = Encoding.UTF8.GetString(cWsReceiveBuffer.ToArray());
+                    cWsReceivedData = cWsReceivedData.Substring(0, cWsReceivedData.IndexOf('\0'));
+                    Console.WriteLine(cWsReceivedData);
+                    jsonStruct_receivedData jsonData = JsonSerializer.Deserialize<jsonStruct_receivedData>(cWsReceivedData);
+                    Console.WriteLine(jsonData.message);
+                    if(jsonData.message!=null)
+                    {
+                        string command = "";
+                        if (jsonData.message.IndexOf(' ') > -1)
+                        {
+                            command = jsonData.message.Substring(0, jsonData.message.IndexOf(' '));
+                        }
+                        else
+                        {
+                            command = jsonData.message;
+                        }
+                        if (jsonData.post_type == "message" && keyWord.Contains(command + ".command"))
+                        {
+                            byte[] sendBuffer = new byte[4096];
+                            object sendDataParam = null;
+                            StreamReader sr = File.OpenText(command + ".command");
+                            if (sr.ReadLine() != "[ex]")
+                            {
+                                if (jsonData.message_type == "group")
+                                {
+                                    sendDataParam = new jsonStruct_send_group_message { group_id = jsonData.group_id, message = getRandWord(jsonData.message + ".command"), auto_escape = true };
+                                }
+                                if (jsonData.message_type == "private")
+                                {
+                                    sendDataParam = new jsonStruct_send_private_message { user_id = jsonData.user_id, message = getRandWord(jsonData.message + ".command"), auto_escape = true };
+                                }
+                            }
+                            else
+                            {
+                                switch (sr.ReadLine())
+                                {
+                                    case "[append command]":
+                                        {
+                                            string message = jsonData.message;
+                                            message = message.Substring(message.IndexOf(' ') + 1);
+                                            string appendCommand = message.Substring(0, message.IndexOf(' '));
+                                            string data = message.Substring(message.IndexOf(' ') + 1);
+                                            StreamWriter sw = File.AppendText(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + appendCommand + ".command");
+                                            sw.WriteLine(data);
+                                            sw.Dispose();
+                                            if (jsonData.message_type == "group")
+                                            {
+                                                sendDataParam = new jsonStruct_send_group_message { group_id = jsonData.group_id, message ="添加成功", auto_escape = true };
+                                            }
+                                            if (jsonData.message_type == "private")
+                                            {
+                                                sendDataParam = new jsonStruct_send_private_message { user_id = jsonData.user_id, message = "添加成功", auto_escape = true };
+                                            }
+                                            refreshFiles();
+                                            break;
+                                        }
+                                    default:
+                                        break;
+                                }
+                            }
+
+                            jsonStruct_api apiData = new jsonStruct_api { action = "send_msg", param = sendDataParam };
+                            Console.WriteLine($"apidata:{JsonSerializer.Serialize<jsonStruct_api>(apiData)}");
+                            if (sendDataParam != null) await cWS.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonSerializer.Serialize<jsonStruct_api>(apiData))), WebSocketMessageType.Text, true, cts);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ColorWriteLine($"Error:{ex.Message}",ConsoleColor.Red);
+                    //cWS.Dispose();
+                }
+            }
+            static void refreshFiles()
+            {
+                files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.command");
+                foreach (string item in files)
+                {
+                    keyWord.Add(item.Substring(item.LastIndexOf(Path.DirectorySeparatorChar) + 1));
+                    ColorWriteLine($"Found Command:{item.Substring(item.LastIndexOf(Path.DirectorySeparatorChar) + 1)}", ConsoleColor.Yellow);
+                }
+            }
+
+            static void ColorWriteLine(string data, ConsoleColor consoleColor)
+            {
+                Console.ForegroundColor = consoleColor;
+                Console.WriteLine(data);
+                Console.ResetColor();
+            }
+
+            static string getRandWord(string FilePath)
+            {
+                StreamReader sr = File.OpenText(FilePath);
+                List<string> words = new List<string>();
+                string s = "";
+                while((s=sr.ReadLine())!=null)
+                {
+                    
+                    words.Add(s);
+                }
+                Random random = new Random();
+                return words[random.Next(0, words.Count)];
+            }
+        }
+    }
+}
---
+++ jsonDataStructs.cs
@@ -0,0 +1,95 @@
+﻿//TODO:是否需要把各类消息类型拆分开来,通过关键字判断后再进行json解析?
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace jsonDataStructs
+{
+    class jsonStruct_receivedData
+    {
+        public Int64 time{get;set;}
+        public Int64 self_id{ get; set; }
+        /// <summary>
+        /// message notice request meta_event
+        /// 暂时只响应message
+        /// </summary>
+        public string post_type{get;set;}
+        /// <summary>
+        /// private:私聊
+        /// group:群组
+        /// </summary>
+        public string message_type{get;set;}
+        /// <summary>
+        /// 私聊消息:
+        /// friend->好友
+        /// group->群临时会话
+        /// other
+        /// 群消息:
+        /// normal->正常消息
+        /// anonymous->匿名消息
+        /// notice->系统提示
+        /// </summary>
+        public string sub_type{get;set;}
+        public Int64 message_id{get;set;}
+        public Int64 user_id{get;set;}
+        public string message{get;set;}//注意这里消息类型在标准中允许使用字符串和数组两种数据类型,可能需要在服务端配置
+        public string raw_message{get;set;}
+        public Int64 font{get;set;}
+        //public Sender sender{get;set;}
+        //----以下仅限群消息
+        public Int64 group_id{get;set;}
+        /// <summary>
+        /// 匿名消息,若不匿名则为null
+        /// </summary>
+        //public object anonymous{get;set;}
+    }
+    class Sender
+    {
+        Int64 user_id{get;set;}
+        string nickname{get;set;}
+        string sex{get;set;}
+        string age{get;set;}
+        //以下仅限群消息
+        string card{get;set;}
+        string area{get;set;}
+        string level{get;set;}
+        /// <summary>
+        /// owner->群主
+        /// admin->管理员
+        /// member->群员
+        /// </summary>
+        string role{get;set;}
+        string title{get;set;}
+    }
+    class jsonStruct_send_private_message
+    {
+        public Int64 user_id{get;set;}
+        public string message{get;set;}
+        public bool auto_escape { get; set; }//是否解析CQ码
+    }
+    class jsonStruct_send_group_message
+    {
+        public Int64 group_id{get;set;}
+        public string message{get;set;}
+        public bool auto_escape = false;
+    }
+    class jsonStruct_send_message
+    {
+        /// <summary>
+        /// group->群组
+        /// private->私聊
+        /// </summary>
+        public string message_type{get;set;}
+        public Int64 user_id{get;set;}
+        public Int64 group_id{get;set;}
+        public string message{get;set;}
+        public bool auto_escape = true;
+    }
+    class jsonStruct_api
+    {
+        public string action{get;set;}
+        [JsonPropertyName("params")]
+        public object param {get;set;}
+        public Int64 echo{get;set;}
+    }
+
+}
